009a398f9918daeefaea69807143fb50aa8cdeb8
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-15 11:21:41 +0000

    Issue #23: Ulterius-specific DeviceConfigDaemon code protected with USE_ULTERIUS conditionals

--- src/device_config_daemon.cpp
+++ src/device_config_daemon.cpp
@@ -1,6 +1,8 @@
 #include "device_config_daemon.h"
 #include "device_config.h"
+#ifdef USE_ULTERIUS
 #include "ulterius_singleton.h"
+#endif
 #include <assert.h>
 
 
@@ -46,13 +48,17 @@ void DeviceConfigDaemon::run()
 	int depth = 0;
 	int freq = 0;
 	DeviceConfig &config = DeviceConfig::get_instance();
+#ifdef USE_ULTERIUS
 	UlteriusSingleton &controller = UlteriusSingleton::get_instance();
+#endif
 	while (running)
 	{
+#ifdef USE_ULTERIUS
 		if (controller.getParamValue("b-depth", depth))
 			config.set_depth(depth);
 		if (controller.getParamValue("b-freq", freq))
 			config.set_freq(freq);
+#endif
 		// TODO: change to an automated value based e.g. on frame rate
 		std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(100));
 	}
