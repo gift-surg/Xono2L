f211731de0d9901a7588e7e24038475387ccf14d
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-15 11:49:10 +0000

    Issue #23: added superfluous static identifier

--- src/ulterius_controller.cpp
+++ src/ulterius_controller.cpp
@@ -3,22 +3,22 @@
 #ifdef USE_ULTERIUS
 
 
-static bool UlteriusController::start_acquisition(const char *device_ident)
+bool UlteriusController::start_acquisition(const char *device_ident)
 {
     return false;
 }
 
-static bool UlteriusController::stop_acquisition()
+bool UlteriusController::stop_acquisition()
 {
     return false;
 }
 
-static bool UlteriusController::is_acquiring()
+bool UlteriusController::is_acquiring()
 {
     return false;
 }
 
-static bool UlteriusController::get_data(
+bool UlteriusController::get_data(
     uint8_t *data, uint32_t *width, uint32_t *height,
     uint32_t *depth, uint32_t *freq
 )
