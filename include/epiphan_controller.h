9348418fa9b5eedb93ad6cbb7305f3cbd9ab0424
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-18 20:35:33 +0000

    Issue #23: added an EpiphanController class stub

---
+++ include/epiphan_controller.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef USE_EPIPHAN
+
+class EpiphanController
+{
+public:
+    static bool start_acquisition(const char *device_ident);
+
+    static bool stop_acquisition();
+
+    static bool is_acquiring();
+
+    static bool get_data(uint8_t *data, uint32_t *width, uint32_t *height);
+};
+
+#endif // USE_EPIPHAN
