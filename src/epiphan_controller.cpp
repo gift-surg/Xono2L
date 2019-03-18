51493f4212a67d83b782fd56d8c8b5b4e78a2a7c
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-18 20:35:33 +0000

    Issue #23: added an EpiphanController class stub

---
+++ src/epiphan_controller.cpp
@@ -0,0 +1,34 @@
+#include "epiphan_controller.h"
+
+#ifdef USE_EPIPHAN
+
+
+bool EpiphanController::start_acquisition(const char *device_ident)
+{
+    // TODO
+    return false;
+}
+
+
+bool EpiphanController::stop_acquisition()
+{
+    // TODO
+    return false;
+}
+
+
+bool EpiphanController::is_acquiring()
+{
+    // TODO
+    return false;
+}
+
+
+bool EpiphanController::get_data(uint8_t *data, uint32_t *width, uint32_t *height)
+{
+    // TODO
+    return false;
+}
+
+
+#endif // USE_EPIPHAN
