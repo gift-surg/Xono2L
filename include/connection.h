454026f50a6e9e03a6648f2ab6398b416a94b40a
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-18 19:36:24 +0000

    Issue #23: added a Stream class stub for managing connection interface

--- include/connection.h
+++ include/connection.h
@@ -5,3 +5,20 @@ enum Interface
     Ulterius,
     Epiphan,
 };
+
+class Stream
+{
+protected:
+    Interface interface;
+    bool initialised;
+protected:
+    Stream();
+public:
+    ~Stream();
+public:
+    static Stream& get_instance();
+    void set_interface(const char *device_ident);
+    Interface get_interface();
+};
+
+static Stream& stream = Stream::get_instance();
