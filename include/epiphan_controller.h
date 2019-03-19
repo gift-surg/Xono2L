2498f945968b665050084e275fe52849ade33bf1
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-19 17:42:54 +0000

    Issue #23: EpiphanController singleton

--- include/epiphan_controller.h
+++ include/epiphan_controller.h
@@ -4,9 +4,23 @@
 
 #ifdef USE_EPIPHAN
 
+#include "frmgrab.h"
+
+
 class EpiphanController
 {
+protected:
+	FrmGrabber * frame_grabber;
+
+protected:
+	EpiphanController();
+	
 public:
+	~EpiphanController();
+
+public:
+	static EpiphanController& get_instance();
+
     static bool start_acquisition(const char *device_ident);
 
     static bool stop_acquisition();
@@ -15,5 +29,7 @@ public:
 
     static bool get_data(uint8_t *data, uint32_t *width, uint32_t *height);
 };
+
+static EpiphanController& epiphan_controller = EpiphanController::get_instance();
 
 #endif // USE_EPIPHAN
