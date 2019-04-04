3bd3096b8aa78787193e2ac249ea291b15b5bfd1
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-04-04 09:25:31 +0100

    Issue #35: determine capture format based on trailing specifier ("-c" / "-i")

--- include/epiphan_controller.h
+++ include/epiphan_controller.h
@@ -24,6 +24,16 @@ public:
 public:
 	static EpiphanController& get_instance();
 
+    /**
+     * Start acquisition from specified Epiphan port.
+     *
+     * \param device_ident serial number of requested Epiphan port, followed
+     * by "-i" for capturing in intensity mode (grayscale images), or "-c"
+     * for capturing in colour mode (3-byte packed RGB images). Defaults to
+     * colour mode if nothing or any other suffix specified.
+     * \return true if acquisition has been started, false otherwise
+     * \see determine_colour which will ALWAYS return a valid colour space
+     */
     bool start_acquisition(const char *device_ident);
 
     bool stop_acquisition();
@@ -31,6 +41,8 @@ public:
     bool is_acquiring();
 
     bool get_data(uint8_t *data, uint32_t *width, uint32_t *height);
+
+    static V2U_INT32 determine_colour(const char *device_ident);
 };
 
 static EpiphanController& epiphan_controller = EpiphanController::get_instance();
--- src/epiphan_controller.cpp
+++ src/epiphan_controller.cpp
@@ -1,6 +1,7 @@
 #include "epiphan_controller.h"
 
 #include <iostream>
+#include <string>
 
 #ifdef USE_EPIPHAN
 
@@ -28,6 +29,19 @@ EpiphanController& EpiphanController::get_instance()
 }
 
 
+V2U_INT32 determine_colour(const char *device_ident)
+{
+    V2U_INT32 colour_space = V2U_GRABFRAME_FORMAT_RGB24;
+    std::string device_ident_(device_ident);
+    std::string colour_spec = device_ident_.substr(device_ident_.size()-2);
+    if (colour_spec.compare("-c"))
+        colour_space = V2U_GRABFRAME_FORMAT_RGB24;
+    else if (colour_spec.compare("-i"))
+        colour_space = V2U_GRABFRAME_FORMAT_I420;
+    return colour_space;
+}
+
+
 bool EpiphanController::start_acquisition(const char *device_ident)
 {
 	if (is_acquiring())
@@ -43,8 +57,7 @@ bool EpiphanController::start_acquisition(const char *device_ident)
 		return false;
 	}
 
-	V2U_INT32 colour_space = V2U_GRABFRAME_FORMAT_RGB24;
-	flags |= colour_space;
+	flags |= determine_colour(device_ident);
 
 	// TODO: set ROI more intelligently
 	roi.x = 320;
